021bf727003651c4368bd72dde5d00a2959af404
Vance Shipley <vances@motivity.ca>
2011-03-30 16:17:07 -0400

    changed to a BSD Revised form of open source license

--- TCAP/src/ANSI/tcap_tsm_fsm.erl
+++ TCAP/src/ANSI/tcap_tsm_fsm.erl
@@ -1,16 +1,39 @@
 %%% $Id: tcap_tsm_fsm.erl,v 1.3 2005/08/04 09:33:17 vances Exp $
 %%%---------------------------------------------------------------------
-%%% @copyright 2004-2005 Motivity Telecom Inc.
+%%% @copyright 2004-2005 Motivity Telecom
 %%% @author Vance Shipley <vances@motivity.ca> [http://www.motivity.ca]
 %%% @end
 %%%
-%%% Copyright Motivity Telecom Inc. 2004-2005
+%%% Copyright (c) 2004-2005, Motivity Telecom
+%%% 
+%%% All rights reserved.
+%%% 
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions
+%%% are met:
+%%% 
+%%%    - Redistributions of source code must retain the above copyright
+%%%      notice, this list of conditions and the following disclaimer.
+%%%    - Redistributions in binary form must reproduce the above copyright
+%%%      notice, this list of conditions and the following disclaimer in
+%%%      the documentation and/or other materials provided with the 
+%%%      distribution.
+%%%    - Neither the name of Motivity Telecom nor the names of its
+%%%      contributors may be used to endorse or promote products derived
+%%%      from this software without specific prior written permission.
 %%%
-%%% All rights reserved. No part of this computer program(s) may be
-%%% used, reproduced, stored in any retrieval system, or transmitted,
-%%% in any form or by any means, electronic, mechanical, photocopying,
-%%% recording, or otherwise without prior written permission of
-%%% Motivity Telecom Inc.
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%%% A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%%% OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%%% LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+%%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%%
 %%%---------------------------------------------------------------------
 %%%
 %%% @doc Transaction State Machine (TCM) functional block within the
