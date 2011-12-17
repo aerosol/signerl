70be83b1c93d6c0c9f1a75919ec2e05bd3a7f3f5
Harald Welte <laforge@gnumonks.org>
2011-12-17 18:35:14 +0100

    tcap_invocation_sup:start_ism/5 instead of start_ism/1

--- TCAP/src/tcap_invocation_sup.erl
+++ TCAP/src/tcap_invocation_sup.erl
@@ -47,12 +47,12 @@
 -export([init/1]).
 
 %% API to other modules
--export([start_ism/1, start_link/1]).
+-export([start_ism/5, start_link/1]).
 
 init([]) ->
 	{ok,{{one_for_all, 0, 1}, []}}.
 
-start_ism([USAP, DlgId, InvokeID, OpClass, Timeout]) ->
+start_ism(USAP, DlgId, InvokeID, OpClass, Timeout) ->
 	SupRef = list_to_atom("tcap_invocation_sup_" ++ integer_to_list(DlgId)),
 	StartArgs = [USAP, DlgId, InvokeID, OpClass, Timeout],
 	StartFunc = {tcap_ism_fsm, start_link, StartArgs},
