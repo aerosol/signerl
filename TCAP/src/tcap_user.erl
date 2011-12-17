5c0f228e1147778fec6ead3cc8a3355c0fe64c86
Harald Welte <laforge@gnumonks.org>
2011-12-17 18:27:13 +0100

    tcap_user: correctly resolve DHA/TCO
    ets lookups return a tuple, not just the Pid value that we want.

--- TCAP/src/tcap_user.erl
+++ TCAP/src/tcap_user.erl
@@ -50,30 +50,26 @@
 
 -include("tcap.hrl").
 
+get_or_start_dha(TCO, DialgId) ->
+	case ets:lookup(tcap_dha, DialgId) of
+	    [] ->
+		% start new DHA FSM for this dialogue; it will start CCO
+		start_new_dha(TCO, DialgId);
+	    [{DialgId, DHA}] ->
+		DHA
+	end.
+
 % the user (TCU) sends us a primitive.  We decide where to route it
 send_prim(TCO, P={'TC', 'INVOKE', request, Param}) when
 				is_record(Param, 'TC-INVOKE') ->
 	DialgId = Param#'TC-INVOKE'.dialogueID,
-	case ets:lookup(tcap_dha, DialgId) of
-	    [] ->
-		% start new DHA FSM for this dialogue; it will start CCO
-		DHA = start_new_dha(TCO, DialgId);
-	    [DHA] ->
-		ok
-	end,
-	% resolve CCO from DHA
+	DHA = get_or_start_dha(TCO, DialgId),
 	CCO = tcap_dha_fsm:get_cco_pid(DHA),
 	gen_server:cast(CCO, P);
 send_prim(TCO, P={'TC', 'BEGIN', request, Param}) when
 				is_record(Param, 'TC-BEGIN') ->
 	DialgId = Param#'TC-BEGIN'.dialogueID,
-	case ets:lookup(tcap_dha, DialgId) of
-	    [] ->
-		% start new DHA FSM for this dialogue; it will start CCO
-		DHA = start_new_dha(TCO, DialgId);
-	    [DHA] ->
-		    ok
-	end,
+	DHA = get_or_start_dha(TCO, DialgId),
 	gen_fsm:send_event(DHA, P);
 send_prim(_TCO, P) ->
 	{errror, {unknown_prim, P}}.
